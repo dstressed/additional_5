3774cdd6249d00288297a7e15758eb0cd825d285
Dmitry Strezh <36566007+dstressed@users.noreply.github.com>
2018-02-21 19:29:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var arr = str.split('');
+  if (arr.length % 2) return false;
+  outer: for (var i = 0; i < arr.length; i++) {
+      var substr = arr[i] + '' + arr[i + 1];
+      for (var n = 0; n < brack.length; n++) {
+          if (substr == brack[n].join('')) {
+              arr.splice(i, 2);
+              i -= 2;
+              continue outer;
+          }
+      }
+  }
+  if (arr.length) return false;
+  else return true;
+};
